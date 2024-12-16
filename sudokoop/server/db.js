ccfa90c5742be14c7107c55f02ff24256f507d1a
Alex Siroli <alex.siroli@studio.unibo.it>
2024-12-16 12:50:53 +0100

    implemented docker container

--- sudokoop/server/db.js
+++ sudokoop/server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/mongodb', {
+        await mongoose.connect('mongodb://mongo:27017/sudokoopDB', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
