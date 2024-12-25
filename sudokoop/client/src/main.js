4a04f3cfff2d73e776fe4ed10a2338c88431c3ef
Alex Siroli <alex.siroli@studio.unibo.it>
2024-12-25 05:24:02 +0100

    moved app styles

--- sudokoop/client/src/main.js
+++ sudokoop/client/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000/api';
+
+import '@/assets/main.css';
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 createApp(App).use(router).mount('#app');
 export default axios;
