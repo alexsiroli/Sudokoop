680b0b366795695dbbc6afff69448c3d7b3205e9
Alice <alice.mastrilli@studio.unibo.it>
2025-01-14 13:54:34 +0100

    GameWithVite is an extension of Game base class

---
+++ sudokoop/server/models/Game.js
@@ -0,0 +1,42 @@
+const sudokuGen = require('sudoku-gen');
+
+class Game {
+    constructor(difficult) {
+        this.sudoku = sudokuGen.getSudoku(difficult);
+        this.emptyPlace = this.sudoku.puzzle.match(/-/g).length;
+    }
+
+    insertNumberWithoutCheck(row, col, num) {
+        const index = row * 9 + col;
+        return this.sudoku.puzzle.substring(0, index) + num + this.sudoku.puzzle.substring(index + 1);
+    }
+
+    // Metodo per inserire un numero in una cella
+    insertNumber(row, col, num) {
+        const index = row * 9 + col; // Calcola l'indice corrispondente nella stringa puzzle
+
+        // Verifica che la posizione sia valida e corrisponda a una cella vuota
+        if (this.sudoku.puzzle[index] !== '-') {
+            return 'Posizione non valida: la cella non è vuota.';
+        }
+
+        // Verifica se il numero inserito è corretto
+        if (this.sudoku.solution[index] === num.toString()) {
+            // Aggiorna la stringa puzzle per mostrare il numero inserito
+            this.sudoku.puzzle =
+                this.sudoku.puzzle.substring(0, index) + num + this.sudoku.puzzle.substring(index + 1);
+
+            this.emptyPlace--;
+            if (this.emptyPlace === 0) {
+                console.log("Hai vinto!!");
+                return;
+            }
+
+            return 'Giusto!';
+        } else {
+            return 'Sbagliato! Riprova.';
+        }
+    }
+}
+
+module.exports = Game;
--- sudokoop/server/models/GameWithVite.js
+++ sudokoop/server/models/GameWithVite.js
@@ -1,50 +1,24 @@
-const sudokuGen = require('sudoku-gen');
+const Game = require("./Game");
 
-class GameWithVite {
+class GameWithVite extends Game {
+
   constructor(difficult) {
-    this.sudoku = sudokuGen.getSudoku(difficult);
-    this.emptyPlace = this.sudoku.puzzle.match(/-/g).length;
+    super(difficult);
     this.vite = 3;  // Imposta le vite iniziali a 3
-  }
-
-  insertNumberWithoutCheck(row, col, num) {
-    const index = row * 9 + col;
-    return this.sudoku.puzzle.substring(0, index) + num + this.sudoku.puzzle.substring(index + 1);
   }
 
   // Metodo per inserire un numero in una cella
   insertNumber(row, col, num) {
-    const index = row * 9 + col; // Calcola l'indice corrispondente nella stringa puzzle
+    const result = super.insertNumber(row, col, num); // Chiama il metodo della classe padre
 
-    // Verifica che la posizione sia valida e corrisponda a una cella vuota
-    if (this.sudoku.puzzle[index] !== '-') {
-      return 'Posizione non valida: la cella non è vuota.';
-    }
-
-    // Verifica se il numero inserito è corretto
-    if (this.sudoku.solution[index] === num.toString()) {
-      // Aggiorna la stringa puzzle per mostrare il numero inserito
-      this.sudoku.puzzle =
-        this.sudoku.puzzle.substring(0, index) + num + this.sudoku.puzzle.substring(index + 1);
-
-      this.emptyPlace--;
-      if (this.emptyPlace === 0) {
-        console.log("Hai vinto!!");
-        return;
-      }
-
-      return 'Giusto!';
-    } else {
-      // Riduce il numero di vite
-      this.vite--;
-
-      // Se non ci sono più vite, il gioco è terminato
+    if (result === 'Sbagliato! Riprova.') {
+      this.vite--; // Riduci le vite
       if (this.vite === 0) {
-        return 'Hai perso! Vite terminate.';
+        return 'Hai perso! Vite terminate.'; // Gioco terminato
       }
-
-      return 'Sbagliato! Riprova.';
     }
+
+    return result; // Restituisci il risultato originale per gli altri casi
   }
 }
 
