daaf6fb757a7faf6a879c1128c9166210a5c3a53
Alice <alice.mastrilli@studio.unibo.it>
2025-01-14 14:15:08 +0100

    created GameVersus as extension of Game

--- sudokoop/server/models/VersusGame.js
+++ sudokoop/server/models/VersusGame.js
@@ -1,50 +1,33 @@
-const sudokuGen = require('sudoku-gen');
+const Game = require("./Game");
 
-class VersusGame {
-    constructor(difficult, firstTeam, secondTeam) {
-        this.sudoku = sudokuGen.getSudoku(difficult);
-        this.emptyPlace = this.sudoku.puzzle.match(/-/g).length;
-        this.vite = 3;  // Imposta le vite iniziali a 3
+class VersusGame extends Game {
+    constructor(difficult, yellowTeam, blueTeam) {
+        super(difficult);
+        this.yellowTeam = yellowTeam;
+        this.blueTeam = blueTeam;
     }
 
-    insertNumberWithoutCheck(row, col, num) {
-        const index = row * 9 + col;
-        return this.sudoku.puzzle.substring(0, index) + num + this.sudoku.puzzle.substring(index + 1);
-    }
 
     // Metodo per inserire un numero in una cella
-    insertNumber(row, col, num) {
-        const index = row * 9 + col; // Calcola l'indice corrispondente nella stringa puzzle
+    insertNumber(row, col, num, username) {
+        const result = super.insertNumber(row, col, num);
+        if (result === 'Sbagliato! Riprova.') {
+            this.yellowTeam = this.yellowTeam.filter(player => player !== username);
 
-        // Verifica che la posizione sia valida e corrisponda a una cella vuota
-        if (this.sudoku.puzzle[index] !== '-') {
-            return 'Posizione non valida: la cella non è vuota.';
+            // Rimuovi il giocatore dalla squadra blu, se presente
+            this.blueTeam = this.blueTeam.filter(player => player !== username);
+
+            if (this.yellowTeam === 0) {
+                return 'Blu vince'; // Gioco terminato
+            }
+            if (this.blueTeam === 0) {
+                return 'Giallo vince';
+            }
+            return 'Eliminato'
         }
 
-        // Verifica se il numero inserito è corretto
-        if (this.sudoku.solution[index] === num.toString()) {
-            // Aggiorna la stringa puzzle per mostrare il numero inserito
-            this.sudoku.puzzle =
-                this.sudoku.puzzle.substring(0, index) + num + this.sudoku.puzzle.substring(index + 1);
+        return result; // Restituisci il risultato originale per gli altri casi
 
-            this.emptyPlace--;
-            if (this.emptyPlace === 0) {
-                console.log("Hai vinto!!");
-                return;
-            }
-
-            return 'Giusto!';
-        } else {
-            // Riduce il numero di vite
-            this.vite--;
-
-            // Se non ci sono più vite, il gioco è terminato
-            if (this.vite === 0) {
-                return 'Hai perso! Vite terminate.';
-            }
-
-            return 'Sbagliato! Riprova.';
-        }
     }
 }
 
