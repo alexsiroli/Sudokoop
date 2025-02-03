baa14eb452e9d7221078e72db58abc7f092cfb14
Alex Siroli <alex.siroli@studio.unibo.it>
2025-02-03 13:17:09 +0100

    new public IP http://54.177.155.194

--- sudokoop/client/src/main.js
+++ sudokoop/client/src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 import './assets/main.css';
 
-axios.defaults.baseURL = 'http://13.53.206.61:5001/api';
+axios.defaults.baseURL = 'http://http://54.177.155.194:5001/api';
 
 createApp(App).use(router).mount('#app');
 
